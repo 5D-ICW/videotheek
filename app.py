418de7ed2810ec4290c632d3057c9981767b6261
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-03-21 00:00:04 +0100

    Account system

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, redirect, abort
+
+from flask import Flask, render_template, request, url_for, redirect, abort, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 def get_db_connection():
     conn = sqlite3.connect('identifier.sqlite')
     conn.row_factory = sqlite3.Row
     return conn
 
+
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
 
 @app.route('/')
 def home():
-    conn = get_db_connection()
-    variable = conn.execute('SELECT * FROM films').fetchall()
-    conn.close()
     return render_template('home.html')
+
 
 @app.route('/favorites')
 def fav():
@@ -34,6 +38,65 @@ def fav():
         klanten[film_id].sort()
 
     return render_template('fav.html', films=films, klanten=klanten)
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        user = conn.execute('SELECT password, id FROM users WHERE username = ?', [username]).fetchone()
+        conn.close()
+
+        if user and check_password_hash(user[0], password):
+            session["user_id"] = user[1]
+            return redirect(url_for("panel"))
+        else:
+            abort(401)
+
+    return render_template('signin.html')
+
+
+@app.route('/panel')
+def panel():
+    if "user_id" not in session:
+        abort(401)
+
+    return render_template('panel.html')
+
+
+@app.route('/panel/create', methods=['GET', 'POST'])
+def create_user():
+    if "user_id" not in session:
+        abort(401)
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+        print(username, password, hashed_password)
+
+        conn = get_db_connection()
+        user = conn.execute('SELECT * FROM users WHERE username = ?', [username]).fetchone()
+        if user:
+            # TODO: Show error message
+            return abort(500)
+
+        conn.execute('INSERT INTO users (username, password) VALUES (?, ?);', (username, hashed_password))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('panel'))
+
+    return render_template('create-user.html')
+
+@app.route("/signout")
+def signout():
+    session.pop("user_id", None)  # Remove user session
+    flash("You have been logged out.", "info")  # Optional message
+    return redirect(url_for("signin"))  # Redirect to login page
+
 
 if __name__ == '__main__':
     app.run()
