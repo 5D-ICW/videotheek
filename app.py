cc188e214ff2f0473153b49dd07fad8fbf4a1a67
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-03-21 16:38:48 +0100

    Create branch

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def get_db_connection():
 
 
 app = Flask(__name__)
+# noinspection SpellCheckingInspection
 app.secret_key = "vwkmRR6oDO6ug9E5h2rQOwUMFTc="
 
 @app.context_processor
@@ -49,16 +50,20 @@ def fav():
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
 
         conn = get_db_connection()
-        user = conn.execute('SELECT password, id FROM users WHERE username = ?', [username]).fetchone()
+        user = conn.execute('SELECT password, klant_id FROM klanten WHERE email = ?', [email]).fetchone()
+        rol = conn.execute('SELECT rol FROM rol WHERE klant_id = ?', [user[1]]).fetchone()
         conn.close()
 
         if user and check_password_hash(user[0], password):
             session["user_id"] = user[1]
-            return redirect(url_for("panel"))
+            session["rol"] = rol[0]
+            if session["rol"] == "ADMIN":
+                return redirect(url_for("panel"))
+            return redirect(url_for("fav"))
         else:
             abort(401)
 
@@ -79,26 +84,94 @@ def create_user():
         abort(401)
 
     if request.method == 'POST':
-        username = request.form['username']
+        name = request.form["name"]
+        email = request.form['email']
         password = request.form['password']
         hashed_password = generate_password_hash(password)
-        print(username, password, hashed_password)
 
         conn = get_db_connection()
-        user = conn.execute('SELECT * FROM users WHERE username = ?', [username]).fetchone()
+        user = conn.execute('SELECT * FROM klanten WHERE email = ?', [email]).fetchone()
         if user:
             # TODO: Show error message
             return abort(500)
 
-        conn.execute('INSERT INTO users (username, password) VALUES (?, ?);', (username, hashed_password))
+        conn.execute('INSERT INTO klanten (naam, email, password) VALUES (?, ?, ?);', (name, email, hashed_password))
         conn.commit()
         conn.close()
         return redirect(url_for('panel'))
 
     return render_template('create-user.html')
 
+@app.route('/panel/delete', methods=['GET', 'POST'])
+def delete_user():
+    if "user_id" not in session:
+        abort(401)
+
+    if request.method == 'POST':
+        username = request.form['username']
+        if username == "admin":
+            abort(500)
+
+        conn = get_db_connection()
+        user = conn.execute('SELECT * FROM klanten WHERE username = ?', [username]).fetchone()
+        if not user:
+            # TODO: Show error message
+            return abort(500)
+
+        conn.execute('DELETE FROM klanten WHERE username = ?;', [username])
+        conn.commit()
+        conn.close()
+        return redirect(url_for('panel'))
+
+    return render_template('delete-user.html')
+
+@app.route('/panel/add', methods=['GET', 'POST'])
+def add_movie():
+    if "user_id" not in session:
+        abort(401)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        genre = request.form['genre']
+        year = request.form['year']
+        director = request.form['director']
+
+        conn = get_db_connection()
+        movie = conn.execute('SELECT * FROM films WHERE titel = ?', [title]).fetchone()
+        if movie:
+            # TODO: Show error message
+            return abort(500)
+
+        conn.execute('INSERT INTO films (titel, genre, releasejaar, regisseur) VALUES (?, ?, ?, ?)', (title, genre, year, director))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('panel'))
+
+    return render_template('add-movie.html')
+
+@app.route('/panel/remove', methods=['GET', 'POST'])
+def remove_movie():
+    if "user_id" not in session:
+        abort(401)
+
+    if request.method == 'POST':
+        title = request.form['title']
+
+        conn = get_db_connection()
+        user = conn.execute('SELECT * FROM films WHERE titel = ?', [title]).fetchone()
+        if not user:
+            # TODO: Show error message
+            return abort(500)
+
+        conn.execute('DELETE FROM films WHERE titel = ?;', [title])
+        conn.commit()
+        conn.close()
+        return redirect(url_for('panel'))
+
+    return render_template('remove-movie.html')
+
 @app.route("/signout")
-def signout():
+def sign_out():
     session.pop("user_id", None)  # Remove user session
     return redirect(url_for("signin"))
 
